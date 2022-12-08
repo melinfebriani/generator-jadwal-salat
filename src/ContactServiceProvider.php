39f81c5326c26da85afd4a5a8c4a00c935c9e678
Melin <melinfebriani19@gmail.com>
2022-12-08 17:13:37 +0700

    update

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kelompok5\GeneratorJadwalSholat;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider {
+    public function boot() {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'jadwalSalat'); 
+        $this->loadViewsFrom(__DIR__.'/views/components', 'header'); 
+        $this->publishes([
+            __DIR__.'/views' => public_path('vendor/courier'),
+        ], 'public');       
+    }
+}
+
