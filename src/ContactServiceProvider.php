23e899f71b698fa951965f758231c4e7fa262772
Melin <melinfebriani19@gmail.com>
2022-12-08 18:14:57 +0700

    update

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -7,8 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class ContactServiceProvider extends ServiceProvider {
     public function boot() {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'jadwalSalat'); 
-        $this->loadViewsFrom(__DIR__.'/views/components', 'header'); 
+        $this->loadViewsFrom(__DIR__.'/views', 'jadwalSalat');  
         $this->publishes([
             __DIR__.'/views' => public_path('vendor/courier'),
         ], 'public');       
